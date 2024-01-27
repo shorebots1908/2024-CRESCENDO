95513749e62396ec1107e79e5a1223e6a3bb7c6a
TimRCAnderson <timothyrcanderson@gmail.com>
2024-01-27 12:53:14 -0500

    added control sanitization (TODO) and intake modifications (TODO)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,7 @@
  * TODO: apriltags reading, better camera stuff
  * TODO: driving tuning
  * TODO: 2 camera pipelines, rings and apriltags tracking
+ * TODO: implement modifyaxis function on controller inputs for drivng. 
  */
 
 package frc.robot;
@@ -142,5 +143,28 @@ public class RobotContainer {
 
     // Run path following command, then stop at the end.
     return swerveControllerCommand.andThen(() -> m_robotDrive.drive(0, 0, 0, false, false));
+  }
+
+  //functions to smooth controller input
+  private static double deadband(double value, double deadband) {
+    if (Math.abs(value) > deadband) {
+      if (value > 0.0) {
+        return (value - deadband) / (1.0 - deadband);
+      } else {
+        return (value + deadband) / (1.0 - deadband);
+      }
+    } else {
+      return 0.0;
+    }
+  }
+
+  private static double modifyAxis(double value) {
+    // Deadband
+    value = deadband(value, 0.05);
+
+    // Square the axis, keep its sign.
+    value = Math.copySign(value * value, value);
+
+    return value;
   }
 }
