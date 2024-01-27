608927e868388597b73f662be244662393510e47
Thomas <dgbroyt@gmail.com>
2024-01-27 08:01:54 -0800

    initial commit

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,41 @@
+package frc.robot.subsystems;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import edu.wpi.first.wpilibj.AnalogInput;
+
+public class IntakeSubsystem extends SubsystemBase {
+      private CANSparkMax m_intake1 = new CANSparkMax(9, MotorType.kBrushless);
+      private CANSparkMax m_intake2 = new CANSparkMax(10, MotorType.kBrushless);
+      private double intakeSpeed = 0;
+      private double intakeReverse = 0;
+      private boolean intakeOn = false;
+      private AnalogInput feedSensor;
+
+
+
+    public IntakeSubsystem() {
+        m_intake1.setIdleMode(IdleMode.kBrake);
+        intakeSpeed = SmartDashboard.getNumber("Intake Speed", intakeSpeed);
+        intakeReverse = SmartDashboard.getNumber("Reverse Intake", intakeReverse);
+        intakeOn = SmartDashboard.getBoolean("Intake On", intakeOn);
+        feedSensor = new AnalogInput(1);
+
+    }
+    public void runIntake() {
+        intakeSpeed = 0.7;
+        intakeReverse = 0;
+        intakeOn = true;
+        
+    }
+
+
+
+}
