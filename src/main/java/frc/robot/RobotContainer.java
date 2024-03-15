8c6a62c1cdf568bf0e1f9ac66203776447ff4196
TimRCAnderson <timothyrcanderson@gmail.com>
2024-03-14 21:25:47 -0400

    tweakign autonomous distance

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -286,7 +286,7 @@ if (alliance.isPresent() && alliance.get() == Alliance.Red) {
         .setKinematics(DriveConstants.kDriveKinematics);
 
     // An example trajectory to follow. All units in meters.
-    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(List.of(new Pose2d(0, 0, new Rotation2d(0)),new Pose2d(2.2, 0, new Rotation2d(0))
+    Trajectory exampleTrajectory = TrajectoryGenerator.generateTrajectory(List.of(new Pose2d(0, 0, new Rotation2d(0)),new Pose2d(2.7, 0, new Rotation2d(0))
       ),
         config);
 
